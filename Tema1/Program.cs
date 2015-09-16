b77728384369ee498741fbffb5146374423241e5
loredanasilaghi <loredana.silaghi@gmail.com>
2015-09-17 01:28:48 +0300

    Solved Problems: 1, 2, 5, 8, 9

---
+++ Tema1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1
+{
+    public class Program
+    {
+        public int Problem1(int m, int n, int a)
+        {
+            double x = Math.Round((double)m / a) + Math.Round((double)n / a);
+            return Convert.ToInt32(x);
+        }
+
+        public string Problem2(int x)
+        {
+            int pos1 = -1;
+            int pos2 = -1;
+            int pos3 = -1;
+            x--;
+
+            List<string> letters = new List<string> { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            pos2 = x / 26 - 1;
+            pos3 = x % 26;
+            if (pos2 >= 26)
+            {
+                pos1 = pos2 / 26 -1;
+            }
+            string result;
+            if (pos1 >= 0)
+            {
+                pos2 -= 26;
+                result = letters[pos1] + letters[pos2] + letters[pos3];
+            }
+            else if (pos2 >= 0)
+            {
+                result = letters[pos2] + letters[pos3];
+            }
+            else
+            {
+                result = letters[pos3];
+            }
+            return result;
+        }
+
+        public string Problem5(int x)
+        {
+            if (x % 2 == 0)
+            {
+                return "DA";
+            }
+            else
+            {
+                return "NU";
+            }
+        }
+
+        public int Problem8(int x, int y, int z, int q, int w)
+        {
+            return (z * q * w) / (x * y);
+        }
+
+        public List<int> Problem9(int n, int x)
+        {
+            int a = n / (x + 1);
+            int r = (n * x) / (x + 1);
+            List<int> list = new List<int>();
+            list.Add(a);
+            list.Add(r);
+            return list;
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Tema1UnitTests/UnitTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema1UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        Tema1.Program program = new Tema1.Program();
+
+        [TestMethod]
+        public void TestProblem1()
+        {
+            int m = 6;
+            int n = 6;
+            int a = 4;
+            Assert.AreEqual(program.Problem1(m,n,a), 4);
+        }
+
+        [TestMethod]
+        public void TestProblem2()
+        {
+            int a = 3;
+            Assert.AreEqual(program.Problem2(a), "C");
+            int b = 52;
+            Assert.AreEqual(program.Problem2(b), "AZ");
+            int c = 703;
+            Assert.AreEqual(program.Problem2(c), "AAA");
+        }
+
+        [TestMethod]
+        public void TestProblem5()
+        {
+            int x = 6;
+            Assert.AreEqual(program.Problem5(x), "DA");
+            int y = 7;
+            Assert.AreEqual(program.Problem5(y), "NU");
+        }
+
+        [TestMethod]
+        public void TestProblem8()
+        {
+            int x = 2;
+            int y = 3;
+            int z = 4;
+            int q = 5;
+            int w = 6;
+            Assert.AreEqual(program.Problem8(x, y, z, q, w), 20);
+        }
+        [TestMethod]
+        public void TestProblem9()
+        {
+            int n = 9;
+            int x = 2;
+            List<int> list = program.Problem9(n, x);
+            Assert.AreEqual(list[0], 3);
+            Assert.AreEqual(list[1], 6);
+        }
+    }
+}
